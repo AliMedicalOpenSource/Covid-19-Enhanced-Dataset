af8b052c709b9a09a0e659fd4d0fb12d0ce5eb8a
aaronali <aaronali@hotmail.com>
2020-05-07 23:15:53 -0300

    Daily Updates

--- scripts/getData.bat
+++ scripts/getData.bat
@@ -1,31 +1,31 @@
 mkdir temp
-curl https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/population.json  > temp/au-nsw-population.json
-curl  https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/data_tests.json  >> temp/au-nsw-tests.json
-curl  https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/agedata.json  >> temp/au-nsw-agedata.json
-curl  https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/usecase2.json > temp/au-nsw-case2.json
-curl https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/data_cases.json >> temp/au-nsw-case.json
-curl  https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/fatalitiesdata.json  >> temp/au-nsw-fatalities.json
 mkdir ..\raw
 mkdir ..\raw\au
 mkdir ..\raw\au\nsw
-copy temp\ ..\raw\au\nsw
-del /s /q temp\
+mkdir ..\raw
+mkdir ..\raw\ca
+mkdir ..\raw\ca\ns 
 
 mkdir ..\raw
 mkdir ..\raw\usa
 mkdir ..\raw\usa\ga
-curl https://ga-covid19.ondemand.sas.com/docs/ga_covid_data.zip >> temp\us_ga_covid.zip
-unzip ..\temp\us_ga_covid.zip 
-copy *.csv ..\raw\usa\ga
-del /s /q ..\temp\us_ga_covid.zip
-del /s /q *.csv
-curl  https://novascotia.ca/coronavirus/data/COVID-19-data.csv >> temp\ca_ns_covid.csv
+curl https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/population.json  >  ..\raw\au\nsw\au-nsw-population.json
+curl  https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/data_tests.json  >>  ..\raw\au\nsw\au-nsw-tests.json
+curl  https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/agedata.json  >>  ..\raw\au\nsw\au-nsw-agedata.json
+curl  https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/usecase2.json >  ..\raw\au\nsw\au-nsw-case2.json
+curl https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/data_cases.json >>  ..\raw\au\nsw\au-nsw-case.json
+curl  https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/fatalitiesdata.json  >>  ..\raw\au\nsw\au-nsw-fatalities.json
 
-mkdir ..\raw
-mkdir ..\raw\ca
-mkdir ..\raw\ca\ns
-copy temp\ ..\raw\ca\ns
-del /s /q temp\
-git add  ..\.
-git commit -m "Auto Update"
-git push -u origin master
+
+
+
+curl https://ga-covid19.ondemand.sas.com/docs/ga_covid_data.zip >> ..\raw\usa\ga\us_ga_covid.zip
+unzip ..\raw\usa\ga\us_ga_covid.zip -d ..\raw\usa\ga\
+ 
+
+curl  https://novascotia.ca/coronavirus/data/COVID-19-data.csv >> ..\raw\ca\ns\ca_ns_covid.csv
+
+ 
+rem git add  ..\.
+rem git commit -m "Auto Update"
+rem git push -u origin master
