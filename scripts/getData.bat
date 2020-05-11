042b1b5bba39fe7dda1b1e8bdcb4a6d6800fc74c
aaronali <aaronali@hotmail.com>
2020-05-10 23:34:58 -0300

    Auto Update

--- scripts/getData.bat
+++ scripts/getData.bat
@@ -20,7 +20,7 @@ curl  https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/fa
 curl https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/resource/ed270bb8-340b-41f9-a7c6-e8ef587e6d11/download/covidtesting.csv >> ..\raw\ca\on\covidtesting.csv
 curl  https://novascotia.ca/coronavirus/data/COVID-19-data.csv >> ..\raw\ca\ns\ca_ns_covid.csv
 curl https://data.ontario.ca/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&limit=100000 >> ..\raw\ca\on\covidcases.json
-
+curl https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposcovidloc.csv >> ..\raw\ca\on\conposcovidloc.csv
 curl https://santesaglac.gouv.qc.ca/medias/2020/05/%C3%89tat-situation-M%C3%89DIAS-2020-05-08.pdf >> ..\raw\ca\qu\quebec-santesaglac.pdf
 curl https://www.ciusss-capitalenationale.gouv.qc.ca/sites/default/files/00-CORONAVIRUS/Communique/2020-05-08_communique_bilan.pdf >> ..\raw\ca\qu\
 
