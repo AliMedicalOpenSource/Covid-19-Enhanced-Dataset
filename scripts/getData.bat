142607eda41ab62081d34bf998e6258c9906ee7c
aaronali <aaronali@hotmail.com>
2020-05-12 21:18:08 -0300

    Auto Update

--- scripts/getData.bat
+++ scripts/getData.bat
@@ -7,6 +7,7 @@ mkdir ..\raw\ca
 mkdir ..\raw\ca\ns 
 mkdir ..\raw\ca\on
 mkdir ..\raw\ca\qu
+mkdir ..\raw\ca\bc
 
 mkdir ..\raw
 mkdir ..\raw\usa
@@ -24,7 +25,7 @@ curl 'https://data.ontario.ca/api/3/action/datastore_search?resource_id=455fd63b
 curl https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposcovidloc.csv >> ..\raw\ca\on\conposcovidloc.csv
 curl https://santesaglac.gouv.qc.ca/medias/2020/05/%C3%89tat-situation-M%C3%89DIAS-2020-05-08.pdf >> ..\raw\ca\qu\quebec-santesaglac.pdf
 curl https://www.ciusss-capitalenationale.gouv.qc.ca/sites/default/files/00-CORONAVIRUS/Communique/2020-05-08_communique_bilan.pdf >> ..\raw\ca\qu\
-
+curl http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Dashboard_Case_Details.csv >> ..\raw\ca\bc\BCCDC_COVID19_Dashboard_Case_Details.csv
 
 curl https://ga-covid19.ondemand.sas.com/docs/ga_covid_data.zip >> ..\raw\usa\ga\us_ga_covid.zip
 unzip ..\raw\usa\ga\us_ga_covid.zip -d ..\raw\usa\ga\
