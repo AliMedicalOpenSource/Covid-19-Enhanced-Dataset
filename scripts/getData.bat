58b2d1381bfc53f097f406119971204f94e9d1fd
aaronali <aaronali@hotmail.com>
2020-05-26 21:19:00 -0300

    Auto Update

--- scripts/getData.bat
+++ scripts/getData.bat
@@ -1,4 +1,5 @@
 mkdir temp
+
 mkdir ..\raw
 mkdir ..\raw\au
 mkdir ..\raw\au\nsw
@@ -12,21 +13,31 @@ mkdir ..\raw
 mkdir ..\raw\usa
 mkdir ..\raw\usa\ga
 mkdir ..\raw\usa\ak
-curl https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/population.json  >  ..\raw\au\nsw\au-nsw-population.json
+del ..\raw\au\nsw\au-nsw-population.json
+curl https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/population.json  >>  ..\raw\au\nsw\au-nsw-population.json
+del ..\raw\au\nsw\au-nsw-tests.json
 curl https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/data_tests.json  >>  ..\raw\au\nsw\au-nsw-tests.json
+del >  ..\raw\au\nsw\au-nsw-agedata.json
 curl https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/agedata.json  >>  ..\raw\au\nsw\au-nsw-agedata.json
-curl https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/usecase2.json >  ..\raw\au\nsw\au-nsw-case2.json
+del ..\raw\au\nsw\au-nsw-case2.json
+curl https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/usecase2.json >>  ..\raw\au\nsw\au-nsw-case2.json
+del ..\raw\au\nsw\au-nsw-case.json
 curl https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/data_cases.json >>  ..\raw\au\nsw\au-nsw-case.json
+del  ..\raw\au\nsw\covid-19-cases-by-notification-date-location-and-likely-source-of-infection.csv
 curl https://data.nsw.gov.au/data/dataset/97ea2424-abaf-4f3e-a9f2-b5c883f42b6a/resource/2776dbb8-f807-4fb2-b1ed-184a6fc2c8aa/download/covid-19-cases-by-notification-date-location-and-likely-source-of-infection.csv >> ..\raw\au\nsw\covid-19-cases-by-notification-date-location-and-likely-source-of-infection.csv
+del  ..\raw\au\nsw\au-nsw-fatalities.json
 curl https://nswdac-np-covid-19-postcode-heatmap.azurewebsites.net/datafiles/fatalitiesdata.json  >>  ..\raw\au\nsw\au-nsw-fatalities.json
+del  ..\raw\ca\on\covidtesting.csv
 curl https://data.ontario.ca/dataset/f4f86e54-872d-43f8-8a86-3892fd3cb5e6/resource/ed270bb8-340b-41f9-a7c6-e8ef587e6d11/download/covidtesting.csv >> ..\raw\ca\on\covidtesting.csv
+del  ..\raw\ca\ns\ca_ns_covid.csv
 curl https://novascotia.ca/coronavirus/data/COVID-19-data.csv >> ..\raw\ca\ns\ca_ns_covid.csv
-curl 'https://data.ontario.ca/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&limit=100000'  >  ..\raw\ca\on\covidcases.json
+del   ..\raw\ca\on\covidcases.json
+curl 'https://data.ontario.ca/api/3/action/datastore_search?resource_id=455fd63b-603d-4608-8216-7d8647f43350&limit=100000'  >>  ..\raw\ca\on\covidcases.json
+del ..\raw\ca\on\conposcovidloc.csv
 curl https://data.ontario.ca/dataset/f4112442-bdc8-45d2-be3c-12efae72fb27/resource/455fd63b-603d-4608-8216-7d8647f43350/download/conposcovidloc.csv >> ..\raw\ca\on\conposcovidloc.csv
-curl https://santesaglac.gouv.qc.ca/medias/2020/05/%C3%89tat-situation-M%C3%89DIAS-2020-05-08.pdf >> ..\raw\ca\qu\quebec-santesaglac.pdf
-curl https://www.ciusss-capitalenationale.gouv.qc.ca/sites/default/files/00-CORONAVIRUS/Communique/2020-05-08_communique_bilan.pdf >> ..\raw\ca\qu\
+del   ..\raw\ca\bc\BCCDC_COVID19_Dashboard_Case_Details.csv
 curl http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Dashboard_Case_Details.csv >> ..\raw\ca\bc\BCCDC_COVID19_Dashboard_Case_Details.csv
-
+del ..\raw\au\nsw\covid19.csv
 curl https://www.health.nsw.gov.au/Infectious/covid-19/Pages/lhd-recovery.csv >> ..\raw\au\nsw\covid19.csv
 curl https://www.health.nsw.gov.au/Infectious/covid-19/Pages/lhd-recovery.csv >> ..\raw\au\nsw\covid19.%date%%.csv
 rem curl https://services1.arcgis.com/WzFsmainVTuD5KML/arcgis/rest/services/Geographical_Distribution_of_Tests/FeatureServer/0/query?where=1%3D1&objectIds=&time=&resultType=none&outFields=*&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&sqlFormat=none&f=pjson&token=  >> ../raw/usa/ak/Geographical_Distribution_of_Tests.json
